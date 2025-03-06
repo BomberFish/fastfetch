955a1345c84c3903b433356cf0a338a40cd42f9c
BomberFish <bomberfish@bomberfish.ca>
2025-03-06 14:44:14 -0500

    add procursus apt support

--- src/detection/packages/packages_apple.c
+++ src/detection/packages/packages_apple.c
@@ -79,10 +79,54 @@ static uint32_t getNixPackages(FFstrbuf* baseDir, const char* dirname)
     return num_elements;
 }
 
+
+static uint32_t getNumStringsImpl(const char* filename, const char* needle)
+{
+    FF_STRBUF_AUTO_DESTROY content = ffStrbufCreate();
+    if (!ffReadFileBuffer(filename, &content))
+        return 0;
+
+    uint32_t count = 0;
+    char *iter = content.chars;
+    size_t needleLength = strlen(needle);
+    while ((iter = memmem(iter, content.length - (size_t)(iter - content.chars), needle, needleLength)) != NULL)
+    {
+        ++count;
+        iter += needleLength;
+    }
+
+    return count;
+}
+
+static uint32_t getNumStrings(FFstrbuf* baseDir, const char* filename, const char* needle, const char* packageId)
+{
+    uint32_t baseDirLength = baseDir->length;
+    ffStrbufAppendS(baseDir, filename);
+
+    FF_STRBUF_AUTO_DESTROY cacheDir = ffStrbufCreate();
+    FF_STRBUF_AUTO_DESTROY cacheContent = ffStrbufCreate();
+
+    uint32_t num_elements;
+    if (ffPackagesReadCache(&cacheDir, &cacheContent, baseDir->chars, packageId, &num_elements))
+    {
+        ffStrbufSubstrBefore(baseDir, baseDirLength);
+        return num_elements;
+    }
+
+    num_elements = getNumStringsImpl(baseDir->chars, needle);
+    ffStrbufSubstrBefore(baseDir, baseDirLength);
+
+    ffPackagesWriteCache(&cacheDir, &cacheContent, num_elements);
+
+    return num_elements;
+}
+
 void ffDetectPackagesImpl(FFPackagesResult* result, FFPackagesOptions* options)
 {
     if (!(options->disabled & FF_PACKAGES_FLAG_BREW_BIT)) getBrewPackages(result);
     if (!(options->disabled & FF_PACKAGES_FLAG_MACPORTS_BIT)) result->macports = getMacPortsPackages();
+	FF_STRBUF_AUTO_DESTROY dpkgDir = ffStrbufCreateS("/opt/procursus");
+	if (!(options->disabled & FF_PACKAGES_FLAG_DPKG_BIT)) result->dpkg += getNumStrings(&dpkgDir, "/var/lib/dpkg/status", "Status: install ok installed", "dpkg");
     if (!(options->disabled & FF_PACKAGES_FLAG_NIX_BIT))
     {
         FF_STRBUF_AUTO_DESTROY baseDir = ffStrbufCreateS(FASTFETCH_TARGET_DIR_ROOT);
